d7f429d72872151a9901d20034fc81500fc1bfe4
Emerson Oliveira <memensoamo@gmail.com>
2021-02-19 14:58:41 -0300

    mensagem de erro

--- lib/exceptions/auth_exceptions.dart
+++ lib/exceptions/auth_exceptions.dart
@@ -6,10 +6,50 @@ class AuthException implements Exception {
     "EMAIL_NOT_FOUND": "Não há registro de usuário correspondente a este identificador. O usuário pode ter sido excluído.",
     "INVALID_PASSWORD": "A senha é inválida ou o usuário não possui uma senha.",
     "USER_DISABLED": "A conta do usuário foi desabilitada por um administrador.",
+
+    //a partir do FiresStone
+    'ERROR_OPERATION_NOT_ALLOWED' : 'contas de e-mail e senha não estão ativadas',
   };
 
   final String key;
   AuthException(this.key);
+
+  @override
+  String toString() {
+    if (errors.containsKey(key)) {
+      return errors[key];
+    } else {
+      return 'Ocorreu um erro na autenticação.';
+    }
+  }
+}
+
+
+class PlatformException implements Exception {
+  static const Map<String, String> errors = {
+    //a partir do FiresStone
+    'ERROR_WRONG_PASSWORD':
+             'Senha errada',
+           
+          'ERROR_USER_DISABLED':
+              'Usuário desativado pelo administrador. Entre em contato com scooterriderbrasil@gmail.com',
+            
+           'ERROR_TOO_MANY_REQUESTS':
+             'Muitas tentativas falhas de Login, tente tente mais tarde!',
+            
+           'ERROR_INVALID_EMAIL':
+             'Endereço de e-mail inválido',
+            
+           'ERROR_USER_NOT_FOUND':
+             'Usuário não encontrado',
+            
+           'ERROR_OPERATION_NOT_ALLOWED':
+             'Ocorreu um erro na autenticação.',
+            
+  };
+
+  final String key;
+  PlatformException(this.key);
 
   @override
   String toString() {
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scooter_rider_brasil/components/auth_card.dart';
 import 'package:scooter_rider_brasil/providers/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScrenn extends StatefulWidget {
   @override
@@ -8,15 +10,53 @@ class AuthScrenn extends StatefulWidget {
 }
 
 class _AuthScrennState extends State<AuthScrenn> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   bool showLogo = true;
   @override
   Widget build(BuildContext context) {
-    void _handleSubmit(Auth authData) {
-      if (authData.isLogin) {
-        //  print('Está no login');
-      } else {
-        //  print('Está no cadastro');
+    final _auth = FirebaseAuth.instance;
 
+    Future<void> _handleSubmit(Auth authData) async {
+      try {
+        if (authData.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+            email: authData.emailUser.trim(), password: authData.password);
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+            email: authData.emailUser.trim(), password: authData.password);
+      }
+      } on PlatformException catch (err) {
+
+        // final msg = err.message ?? 'Ocorreu um erro! Verifique suas credencias.';
+        var msg  ;
+        print(err.code);
+
+        switch (err.code) {
+          case 'ERROR_WRONG_PASSWORD':
+            msg = 'Senha errada';
+            break;
+          case 'ERROR_USER_DISABLED':
+            msg = 'Usuário desativado pelo administrador. Entre em contato com scooterriderbrasil@gmail.com';
+            break;
+          case 'ERROR_TOO_MANY_REQUESTS':
+            msg = 'Muitas tentativas. Fale conosco pelo e-mail: scooterriderbrasil@gmail.com ou tente tente mais tarde!';
+            break;
+          case 'ERROR_INVALID_EMAIL':
+            msg = 'Endereço de e-mail inválido';
+            break;
+          case 'ERROR_USER_NOT_FOUND':
+            msg = 'Usuário não encontrado';
+            break;
+          case 'ERROR_OPERATION_NOT_ALLOWED':
+            msg = 'Ocorreu um erro na autenticação.';
+            break;
+          default: msg = 'Tentativa de autenticação falhou.\n- Verifique sua internet';
+        }
+
+          _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(msg),backgroundColor: Theme.of(context).errorColor,duration: Duration(seconds: 7),));
+
+      }catch(err){
+        print('=>>>>>$err');
       }
     }
 
@@ -27,6 +67,7 @@ class _AuthScrennState extends State<AuthScrenn> {
     }
 
     return Scaffold(
+      key: _scaffoldKey,
       body: Stack(
         children: [
           Container(
@@ -64,6 +105,7 @@ class _AuthScrennState extends State<AuthScrenn> {
                       'Scooter Rider',
                       style: TextStyle(
                         color: Colors.black87,
+                        fontWeight: FontWeight.w300,
                         letterSpacing: .5,
                         fontSize: MediaQuery.of(context).size.width * 0.1,
                       ),
